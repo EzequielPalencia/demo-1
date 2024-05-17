aebec66d04ba2ed57bfd0aae880ec8a55f6d2dcd
CamiloProgramadix <camilocabrales2005@gmail.com>
2024-05-16 21:00:20 -0500

    Se agrego actualizacion Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,20 +4,36 @@ import './Header.css'; // Importa el archivo de estilos CSS para el encabezado
 import Logo_Rivera from "./img/Logo-removebg-preview.png";
 
 const Header = () => {
- 
 
- 
-   
-    return (
+  const isLoggedIn = () => {
+    const token = localStorage.getItem('token');
+    return token !== null;
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+
+  };
+
+  return (
     <header className="header">
-        <img src={Logo_Rivera} alt="" className="Logo_Rivera" />
+      <img src={Logo_Rivera} alt="Logo Rivera" className="Logo_Rivera" />
       <nav className="nav">
         <ul className="nav-list">
           <li><Link to="/" className="nav-link">Inicio</Link></li>
           <li><Link to="/products" className="nav-link">Productos</Link></li>
-          <li><Link to="/login" className="nav-link">Iniciar sesión</Link></li>
-          <li><Link to="/register" className="nav-link">Registrarse</Link></li>
-          <li><Link to="/cart" className="nav-link">Carrito</Link></li>
+          {!isLoggedIn() && (
+            <>
+              <li><Link to="/login" className="nav-link">Iniciar sesión</Link></li>
+              <li><Link to="/register" className="nav-link">Registrarse</Link></li>
+            </>
+          )}
+          <li><Link to="/cart" className="nav-link"><img src="carritocompras.png" alt="" height="30" /></Link></li>
+          {isLoggedIn() && (
+            <>
+              <a href='' className="nav-link" onClick={logout}>Logout</a>
+            </>
+          )}
         </ul>
       </nav>
     </header>
@@ -25,3 +41,4 @@ const Header = () => {
 }
 
 export default Header;
+
